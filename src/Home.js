27c3aed42d76dfc013acd0f7715f6f2d4495c742
Khairul <me@khairul.my.id>
2019-07-01 00:23:08 +0700

    first app design

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import {Container, Header, Body, Title, Content, Text} from 'native-base';
+import {View, Image} from 'react-native';
+import {
+	Container, Header, Body, Title, Content, Text,
+	Form, Item, Input, Icon, Label, Button
+} from 'native-base';
+
+class AppHeader extends React.Component {
+	render() {
+		return (
+			<Header>
+				<Body>
+					<Title>{this.props.title}</Title>
+				</Body>
+			</Header>
+		);
+	}
+}
 
 export default class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			beratBadan: null,
+			tinggiBadan: null
+		};
+	}
+	
 	render() {
 		return (
 			<Container>
-				<Header>
-					<Body>
-						<Title>Test Hehe</Title>
-					</Body>
-				</Header>
+				<AppHeader title="Kalkulator IMT/BMI" />
 				
 				<Content padder>
-					<Text>test</Text>
+					<View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+						<Image source={require('../assets/weight-loss-illustration.png')} style={{width: 100, height: 100}} />
+					</View>
+					
+					<Form style={{marginTop: 32}}>
+						<Item>
+							<Icon active type="FontAwesome5" name='weight' />
+							<Input placeholder="Berat badan (kg)" value={this.state.beratBadan} onChangeText={(text) => this.setState({beratBadan: text})} />
+						</Item>
+						
+						<Item>
+							<Icon active type="MaterialCommunityIcons" name='human' />
+							<Input placeholder="Tinggi badan (cm)" value={this.state.tinggiBadan} onChangeText={(text) => this.setState({tinggiBadan: text})} />
+						</Item>
+					</Form>
+					
+					<Button style={{marginTop: 32}} full success><Text>Hitung IMT</Text></Button>
 				</Content>
 			</Container>
 		);
